ce8da8ec40f78e27ef7798afe39f819fca1e02b3
ericmartinezg <ericmartinezg05@gmail.com>
2026-02-28 01:59:19 +0100

    Update app.py

--- app.py
+++ app.py
@@ -131,14 +131,14 @@ escudos = {
 col1, col2, col3 = st.columns([2, 1, 2])
 
 with col1:
-    st.markdown("""<img src="{escudos[equipo_local]}" style="width: 5rem; height: 5rem; object-fit: contain;">
+    st.markdown("<img src="{escudos[equipo_local]}" style="width: 5rem; height: 5rem; object-fit: contain;">
     equipo_local = st.selectbox("Seleccionar Equipo Local", equipos, index=0, label_visibility="collapsed")
 
 with col2:
     st.markdown("<div style='height: 50%;'></div><div class='vs-badge'>VS</div>", unsafe_allow_html=True)
 
 with col3:
-    st.markdown("""<img src="{escudos[equipo_local]}" style="width: 5rem; height: 5rem; object-fit: contain;">
+    st.markdown("<img src="{escudos[equipo_local]}" style="width: 5rem; height: 5rem; object-fit: contain;">
     equipo_visitante = st.selectbox("Seleccionar Equipo Visitante", equipos, index=1, label_visibility="collapsed")
 
 st.markdown("<br>", unsafe_allow_html=True)
